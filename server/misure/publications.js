c1557f0104b1ec1f34422c0d45da9135e25dcf05
Paolo Scanferla <paolo.scanferla@mondora.com>
2015-07-21 06:28:31 +0200

    Select siti using either a string or an ObjectId as _id

--- server/misure/publications.js
+++ server/misure/publications.js
@@ -1,5 +1,8 @@
 Meteor.publish("misureBySito", function (sitoId) {
     check(sitoId, String);
-    var sito = Siti.findOne({_id: new Mongo.ObjectID(sitoId)});
+    var sito = Siti.findOne({$or: [
+        {_id: new Mongo.ObjectID(sitoId)},
+        {_id: sitoId}
+    ]});
     return sito && Misure.find({pod: sito.pod});
 });
