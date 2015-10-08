e6311e41c1b20515029bff9c95004f18a5ab06a3
Emanuele Busi <emanuele.busi@mondora.com>
2015-10-08 15:38:54 +0200

    IWWA-11 updated meteor version. Sorted  results

--- server/misure/publications.js
+++ server/misure/publications.js
@@ -1,21 +1,16 @@
 Meteor.publish("misureBySito", function (sitoId) {
     check(sitoId, String);
-    var sito = Siti.findOne({$or: [
-        {_id: new Mongo.ObjectID(sitoId)},
-        {_id: sitoId}
-    ]});
-    return sito && Misure.find({pod: sito.pod});
-});
-
-
-
-Meteor.publish("misureBySitoTwoMonths", function (sitoId) {
-    check(sitoId, String);
-    var sito = Siti.findOne({$or: [
-        {_id: new Mongo.ObjectID(sitoId)},
-        {_id: sitoId}
-    ]});
-    var startingDate = moment(new Date()).subtract(2, 'months')._d;
-    var misure = Misure.find({$and : [{pod: sito.pod}, {data: {$gt: startingDate.toISOString()}}]});
-    return sito && misure;
+    var sito = Siti.findOne({
+        _id: sitoId
+    });
+    if (!sito) {
+        return null;
+    }
+    return Misure.find({
+        pod: sito.pod
+    }, {
+        sort: {
+            data: -1
+        }
+    });
 });
