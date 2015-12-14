d3dfcfc1cdb4e2e8ba9f794776b04bd25ff41522
Emanuele Busi <emanuele.busi@mondora.com>
2015-12-14 14:38:53 +0100

    IWWA-141 changed sites publication

--- server/sites/publications.js
+++ server/sites/publications.js
@@ -1,12 +1,3 @@
-var sitiOptions = {
-    fields: {
-        _id: 1,
-        name: 1,
-        pods: 1,
-        otherSensors: 1
-    }
-};
-
 Meteor.publish("sites", function () {
-    return Sites.find({}, sitiOptions);
+    return Sites.find({});
 });
