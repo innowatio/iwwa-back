cb3fc675513293f3940332c078966f693d6d37ac
Davide Bianchi <davide.bianchi@mondora.com>
2015-12-28 13:00:46 +0100

    Added fixtures for site-month-readings-aggregates

--- server/site-month-readings-aggregates/fixtures.js
+++ server/site-month-readings-aggregates/fixtures.js
@@ -1,10 +1,7 @@
-function randomValue () {
-    const value = [];
-    const lengthOfTheArray = 30*24*12;
-    for (var i=0; i <= lengthOfTheArray ; i++) {
-        value.push((Math.random() * 10).toFixed(1))
-    }
-    return value.join(",");
+function getTime () {
+    const month = new Date().getMonth() + 1;
+    const year = new Date().getYear() + 1900;
+    return `${year}-${month}`;
 }
 
 Meteor.startup(() => {
@@ -12,18 +9,19 @@ Meteor.startup(() => {
         process.env.ENVIRONMENT !== "production" &&
         SiteMonthReadingsAggregates.find().count() === 0
     ) {
-        SiteMonthReadingsAggregates.insert({
-            podId: "podId",
-            month: moment().format("YYYY-MM"),
-            measurements: {
-                "activeEnergy": randomValue(),
-                "reactiveEnergy": randomValue(),
-                "maxPower": randomValue(),
-                "temperature": randomValue(),
-                "humidity": randomValue(),
-                "illuminance": randomValue(),
-                "co2": randomValue()
-            }
-        });
+        var test1 = JSON.parse(
+            Assets.getText("fixtures/site-month-readings-aggregates/test-sites-1.json")
+        );
+        var test2 = JSON.parse(
+            Assets.getText("fixtures/site-month-readings-aggregates/test-sites-2.json")
+        );
+
+        test1._id = `${test1.siteId}-${getTime()}`
+        test1.month = getTime();
+        test2._id = `${test2.siteId}-${getTime()}`
+        test2.month = getTime();
+
+        SiteMonthReadingsAggregates.insert(test1);
+        SiteMonthReadingsAggregates.insert(test2);
     }
 });
