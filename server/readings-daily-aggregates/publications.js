142681d8ae8c96c30c03752426d6124d64e24c9a
Davide Bianchi <davide.bianchi@mondora.com>
2016-01-15 18:07:17 +0100

    Change publication of sensor-daily-aggregates to include source.

--- server/readings-daily-aggregates/publications.js
+++ server/readings-daily-aggregates/publications.js
@@ -1,9 +1,11 @@
-Meteor.publish("dailyMeasuresBySensor", (sensorId, dayStart, dayEnd) => {
+Meteor.publish("dailyMeasuresBySensor", (sensorId, source, dayStart, dayEnd) => {
     check(sensorId, String);
+    check(source, String);
     check(dayStart, String);
     check(dayEnd, String);
     return ReadingsDailyAggregates.find({
         sensorId,
+        source,
         day: {
             $gte: dayStart,
             $lte: dayEnd
