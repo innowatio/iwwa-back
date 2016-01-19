e5adb96d2ef33257ad9a3daa9434bd508e984d48
Davide Bianchi <davide.bianchi@mondora.com>
2016-01-19 17:59:00 +0100

    Changed fixtures of readings-daily-aggregates.

--- server/readings-daily-aggregates/fixtures.js
+++ server/readings-daily-aggregates/fixtures.js
@@ -2,6 +2,37 @@ function getTime (day, monthIndex) {
     return moment.utc().subtract(monthIndex, "month").date(day).format("YYYY-MM-DD");
 }
 
+function getMeasurementsTypes (sensorId) {
+    if (sensorId.indexOf("ANZ") >= 0 || sensorId.indexOf("IT") >= 0) {
+        return ["activeEnergy", "reactiveEnergy", "maxPower"];
+    } else if (sensorId.indexOf("ZTHL") >= 0) {
+        return ["temperature", "humidity", "illuminance"];
+    } else if (sensorId.indexOf("COOV") >= 0) {
+        return ["co2"];
+    } else {
+        return [];
+    }
+}
+
+function getUnitOfMeasurement (measurementType) {
+    switch (measurementType) {
+    case "activeEnergy":
+        return "kWh";
+    case "reactiveEnergy":
+        return "kVArh";
+    case "maxPower":
+        return "VAr";
+    case "temperature":
+        return "°C";
+    case "humidity":
+        return "%";
+    case "illuminance":
+        return "Lux";
+    case "co2":
+        return "ppm";
+    }
+}
+
 function getRandomArbitrary(min, max, source) {
     if (!min || !max) {
         return "";
@@ -13,43 +44,39 @@ function getRandomArbitrary(min, max, source) {
     return value.toFixed(2);
 }
 
-function createData (data, value, source) {
+function createMeasurementValues (measurements, sensorId, source, measurementType) {
     const lengthOfTheArray = 12*24;
-    var objectKeys = [];
-    if (value.indexOf("ANZ") >= 0 || value.indexOf("IT") >= 0) {
-        objectKeys = ["activeEnergy", "reactiveEnergy", "maxPower"];
-    } else if (value.indexOf("ZTHL") >= 0) {
-        objectKeys = ["temperature", "humidity", "illuminance"];
-    } else if (value.indexOf("COOV") >= 0) {
-        objectKeys = ["co2"];
+    var array = [];
+    const measurementsString = measurements[measurementType];
+    const measurementsArray = measurementsString.split(",");
+    for (var i=0; i<lengthOfTheArray; i++) {
+        const measurementsValue = measurementsArray[i];
+        const max = parseFloat(measurementsValue + measurementsValue*10/100);
+        const min = parseFloat(measurementsValue - measurementsValue*10/100);
+        array.push(getRandomArbitrary(max, min, source));
     }
-    return objectKeys.reduce((prev, value) => {
-        var array = [];
-        for (var i=0; i<lengthOfTheArray; i++) {
-            const dataValue = data[value][i];
-            const max = parseFloat(dataValue + dataValue*10/100);
-            const min = parseFloat(dataValue - dataValue*10/100);
-            array.push(getRandomArbitrary(max, min, source));
-        }
-        prev[value] = array.toString();
-        return prev;
-    }, {})
+    return array.join(",");
 }
 
-function insertDataFromJSON (path, sensorName, source) {
+function insertDataFromJSON (path, sensorsIds, source) {
     const numberOfMonth = 2
     const data = JSON.parse(Assets.getText(path));
-    sensorName.map(value => {
-        for (var l=0; l<numberOfMonth; l++) {
-            for (var i=1; i<=moment().subtract(l, "month").daysInMonth(); i++) {
-                const date = getTime(i, l);
-                ReadingsDailyAggregates.insert({
-                    _id: `${source}-${value}-${date}`,
-                    sensorId: value,
-                    source,
-                    day: date,
-                    measurements: createData(data.measurements, value, source),
-                    measurementsDeltaInMs: 300000
+    sensorsIds.map(sensorId => {
+        for (var monthIdx=0; monthIdx<numberOfMonth; monthIdx++) {
+            for (var dayOfMonth=1; dayOfMonth<=moment().subtract(monthIdx, "month").daysInMonth(); dayOfMonth++) {
+                const day = getTime(dayOfMonth, monthIdx);
+                const measurementsTypes = getMeasurementsTypes(sensorId);
+                measurementsTypes.forEach(measurementType => {
+                    ReadingsDailyAggregates.insert({
+                        _id: `${sensorId}-${day}-${source}-${measurementType}`,
+                        sensorId,
+                        day,
+                        source,
+                        measurementType,
+                        measurementValues: createMeasurementValues(data.measurements, sensorId, source, measurementType),
+                        measurementsDeltaInMs: 300000,
+                        unitOfMeasurement: getUnitOfMeasurement(measurementType)
+                    });
                 });
             }
         };
@@ -58,16 +85,17 @@ function insertDataFromJSON (path, sensorName, source) {
 }
 
 /*
-Expected structure of the db:
-{
-    _id: "source-sensorId-day",
-    sensorId = "sensorId",
-    source: ["reading", "forecast"],
-    day = "YYYY-MM-DD",
-    measurements = {},
-    measurementsDeltaInMs = 300000
-
-}
+*   Expected structure of the db:
+*   {
+*       _id: "sensorId-day-source-measurementType",
+*       day = "YYYY-MM-DD",
+*       sensorId = "sensorId",
+*       source: ["reading", "forecast"],
+*       measurementType = "",
+*       measurementsDeltaInMs = 300000,
+*       measurementValues: "",
+*       unitOfMeasurement: ""
+*   }
 */
 Meteor.startup(() => {
     if (
@@ -75,16 +103,16 @@ Meteor.startup(() => {
         ReadingsDailyAggregates.find().count() === 0
     ) {
         const path = "fixtures/readings-daily-aggregates";
-        const sensorTest1 = ["IT001", "ANZ01", "IT002", "ANZ02", "ZTHL01", "ZTHL02",
+        const listOfSensorIdInSiteTest1 = ["IT001", "ANZ01", "IT002", "ANZ02", "ZTHL01", "ZTHL02",
             "ZTHL03", "ZTHL04", "COOV01", "COOV02"];
-        const sensorTest2 = ["IT003", "ANZ03", "IT004", "ANZ04", "ANZ05", "ANZ06",
+        const listOfSensorIdInSiteTest2 = ["IT003", "ANZ03", "IT004", "ANZ04", "ANZ05", "ANZ06",
             "ZTHL05", "ZTHL06", "ZTHL07", "ZTHL08", "ZTHL09", "ZTHL10", "ZTHL11",
             "COOV03", "COOV04", "COOV05"];
         const allowedSource = ["reading", "forecast"];
 
         allowedSource.map(source => {
-            insertDataFromJSON(`${path}/test-sites-value.json`, sensorTest1, source);
-            insertDataFromJSON(`${path}/test-sites-value.json`, sensorTest2, source);
+            insertDataFromJSON(`${path}/test-sites-value.json`, listOfSensorIdInSiteTest1, source);
+            insertDataFromJSON(`${path}/test-sites-value.json`, listOfSensorIdInSiteTest2, source);
         });
     }
 });
