84c3fbd90d74d0141a76cafb235a2e576ddcc934
Davide Bianchi <davide.bianchi@mondora.com>
2016-06-20 10:39:17 +0200

    Fixed users publication for not admin role

--- server/users/publications.js
+++ server/users/publications.js
@@ -1,7 +1,10 @@
 Meteor.publish("users", function () {
     var user = Meteor.users.findOne({_id: this.userId});
-    if (!user || !_.contains(user.roles, "admin")) {
+    if (!user) {
         return null;
+    }
+    if (!_.contains(user.roles, "admin")) {
+        return Meteor.users.find({_id: this.userId});
     }
     return Meteor.users.find({}, {
         fields: {
