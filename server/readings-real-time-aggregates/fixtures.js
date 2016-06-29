c6b8ee4da8a8cddd5eb7119580efd3a464359a25
Manuel Serra <nitroin@hotmail.com>
2016-06-29 16:25:06 +0200

    Updated realtime fixtures

--- server/readings-real-time-aggregates/fixtures.js
+++ server/readings-real-time-aggregates/fixtures.js
@@ -1,12 +1,21 @@
 Meteor.startup(() => {
     if (
-        process.env.ENVIRONMENT !== "production" &&
-        ReadingsRealTimeAggregates.find().count() === 0
+        process.env.ENVIRONMENT !== "production"
     ) {
+        ReadingsRealTimeAggregates.remove({});
         const path = "fixtures/readings-real-time-aggregates";
         const json = JSON.parse(Assets.getText(`${path}/test-realtime-aggregates.json`));
+        const day = moment().format("YYYY-MM-DD");
         json.map(realtime => {
-            ReadingsRealTimeAggregates.insert(realtime);
+            ReadingsRealTimeAggregates.insert({
+                _id: `${realtime.sensorId}-${day}-reading-${realtime.measurementType}`,
+                day: day,
+                measurementType : realtime.measurementType,
+                measurementValue : realtime.measurementValue,
+                measurementTime : realtime.measurementTime,
+                unitOfMeasurement : realtime.unitOfMeasurement,
+                sensorId : realtime.sensorId
+            });
         })
     }
 });
