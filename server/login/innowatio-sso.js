578b56b66375a52e3ab6db8aa1389153a14037f6
Davide Morri <davide.morri@mondora.com>
2016-07-19 20:14:13 +0900

    add checktoken serverside + set token on innowatio sso

--- server/login/innowatio-sso.js
+++ server/login/innowatio-sso.js
@@ -1,12 +1,9 @@
 import {HTTP} from "meteor/http";
-import {getUserId} from "./methods"
 
 Accounts.registerLoginHandler("sso", (options) => {
-
     if (!options.sso) {
         return undefined;
     }
-
     const {username, password} = options.sso;
     const result = HTTP.post("https://sso.innowatio.it/openam/json/authenticate", {
         headers: {
@@ -15,11 +12,99 @@ Accounts.registerLoginHandler("sso", (options) => {
             "Content-Type": "application/json"
         }
     });
-    if (200 === result.statusCode && result.data.tokenId) {
-        return getUserId(result.data.tokenId);
+    const {tokenId} = result.data;
+    if (200 === result.statusCode && tokenId) {
+        if (setTokenOnInnowatioSSO(tokenId)) {
+            return getUserId(tokenId);
+        } else {
+            throw new Meteor.Error(401, "set-innowatio-sso-token-failed");
+        }
     } else {
-        return {
-            error: new Meteor.Error(401, "authentication-failed")
-        };
+        throw new Meteor.Error(401, "authentication-failed");
     }
 });
+
+Accounts.validateLoginAttempt(function (attempt) {
+    if (attempt.type == "resume") {
+        checkToken(attempt.user);
+    }
+    return true;
+});
+
+function checkToken (user) {
+    const result = HTTP.post(`https://sso.innowatio.it/openam/json/sessions/${user.innowatioToken}?_action=validate`);
+    if (200 != result.statusCode || false === result.data.valid) {
+        throw new Meteor.Error(401, "invalid-token");
+    } else {
+        return insertOrUpdateUser(user._id, user.innowatioToken);
+    }
+}
+
+function getUserId (tokenId) {
+    const result = HTTP.post("https://sso.innowatio.it/openam/json/users?_action=idFromSession", {
+        headers: {
+            "iplanetDirectoryPro": tokenId,
+            "Content-Type": "application/json"
+        }
+    });
+    if (200 === result.statusCode) {
+        return insertOrUpdateUser(result.data.id, tokenId);
+    } else {
+        throw new Meteor.Error(400, 'get-userid-failed');
+    }
+}
+
+function insertOrUpdateUser (userId, tokenId) {
+    const user = Meteor.users.findOne({
+        "_id": userId
+    });
+    const userInfo = getUserInfo(userId, tokenId);
+    if (!userInfo) {
+        throw new Meteor.Error(400, 'get-user-info-failed');
+    }
+    if (user) {
+        Meteor.users.update({
+            _id: user._id
+        }, {
+            $set: {
+                "innowatioToken": tokenId
+            }
+        });
+    } else {
+        Meteor.users.insert({
+            _id: userId,
+            "innowatioToken": tokenId,
+            "emails": userInfo.mail
+        });
+    }
+    return {
+        userId: userId
+    }
+}
+
+function getUserInfo (userId, tokenId) {
+    const result = HTTP.get(`https://sso.innowatio.it/openam/json/users/${userId}`, {
+        headers: {
+            "iplanetDirectoryPro": tokenId,
+            "Content-Type": "application/json"
+        }
+    });
+    if (200 === result.statusCode) {
+        return result.data;
+    }
+}
+
+function setTokenOnInnowatioSSO (tokenId) {
+    const result = HTTP.post("https://sso.innowatio.it/tokenId", {
+        data: {
+            "tokenId": tokenId
+        }
+    });
+    console.log(result);
+    return 200 === result.statusCode;
+}
+
+function getTokenFromInnowatioSSO () {
+    const result = HTTP.get("https://sso.innowatio.it/tokenId");
+    console.log(result);
+}
