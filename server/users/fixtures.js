a7014c8431f0acc65bb54b429d191b15e9c616dd
Manuel Serra <nitroin@hotmail.com>
2016-08-04 18:19:57 +0200

    User admin fixtures

---
+++ server/users/fixtures.js
@@ -0,0 +1,24 @@
+Meteor.startup(() => {
+    if (
+        process.env.ENVIRONMENT !== "production" &&
+        countAdmins() === 0
+    ) {
+        insertAdmin();
+    }
+});
+
+function countAdmins() {
+    return Meteor.users.find({
+        _id: "admin"
+    }).count();
+}
+
+function insertAdmin() {
+    Meteor.users.insert({
+        _id: "admin",
+        profile: {},
+        roles: [
+            "admin"
+        ]
+    });
+}
