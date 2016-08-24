d1a3beb2c9d037ce6c3a5545d0ed2e71b9076371
Manuel Serra <nitroin@hotmail.com>
2016-08-24 16:26:51 +0200

    Changed _id for realtime aggregates

--- server/readings-real-time-aggregates/fixtures.js
+++ server/readings-real-time-aggregates/fixtures.js
@@ -8,7 +8,7 @@ Meteor.startup(() => {
         const day = moment().format("YYYY-MM-DD");
         json.map(realtime => {
             ReadingsRealTimeAggregates.insert({
-                _id: `${realtime.sensorId}-${day}-reading-${realtime.measurementType}`,
+                _id: `${realtime.sensorId}-reading-${realtime.measurementType}`,
                 day: day,
                 measurementType : realtime.measurementType,
                 measurementValue : realtime.measurementValue,
