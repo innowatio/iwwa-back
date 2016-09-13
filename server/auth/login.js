93386a2ffd8bcb4f4497595372ce73d4a394d002
Davide Bianchi <davide.bianchi@mondora.com>
2016-09-13 17:17:55 +0200

    Added login logs

--- server/auth/login.js
+++ server/auth/login.js
@@ -1,12 +1,35 @@
 import {HTTP} from "meteor/http";
 import {getSessionInfo, isSingleSignOnEnabled} from "../sso-commons";
+import bunyan from "bunyan";
+import Elasticsearch from "bunyan-elasticsearch";
+
+var esStream = new Elasticsearch({
+    indexPattern: "[meteor-auth-]YYYY.MM.DD",
+    type: "logs",
+    host: process.env.ELASTIC_SEARCH_HOST
+});
+
+esStream.on("error", function (err) {
+    console.log('Elasticsearch Stream Error:', err.stack);
+});
+
+const log = bunyan.createLogger({
+    name: "meteor-auth",
+    streams: [{
+        stream: esStream
+    }, {
+        stream: process.stdout
+    }]
+});
 
 Accounts.registerLoginHandler("sso", (options) => {
     if (!options.sso) {
         return undefined;
     }
-    logFunc("registerLoginHandler");
-    log("options", options);
+    log.info({
+        username: options.sso.username,
+        password: options.sso.password ? "***" : undefined
+    }, "registerLoginHandler [sso]");
     return isSingleSignOnEnabled() ? loginWithCredentials(options.sso) : retrieveUpsertUser(options.sso.username);
 });
 
@@ -14,33 +37,32 @@ Accounts.registerLoginHandler("token", (options) => {
     if (!options.token) {
         return undefined;
     }
-    logFunc("registerLoginHandler");
-    log("options", options);
+    log.info({
+        token: token ? "***" : undefined
+    }, "registerLoginHandler [token]");
     return loginWithToken(options.token);
 });
 
 Accounts.validateLoginAttempt((attempt) => {
-    logFunc("validateLoginAttempt");
-    log("attempt", attempt);
     if (attempt.type === "resume") {
         if (!attempt.user || !attempt.user.services || !attempt.user.services.sso) {
             throw new Meteor.Error(400, "invalid-db-user");
         }
+        log.info({
+            username: attempt.user.services.sso.uid
+        }, "validateLoginAttempt");
         return isSingleSignOnEnabled() ? loginWithToken(attempt.user.services.sso.token) : true;
     }
     return true;
 });
 
 Accounts.onLogout((session) => {
-    logFunc("onLogout");
-    log("session", session);
     if (session && session.user && session.user.services && session.user.services.sso) {
         invalidateToken(session.user.services.sso.token);
     }
 });
 
 function loginWithToken (token) {
-    logFunc("loginWithToken");
     const sessionInfo = getSessionInfo(token);
     if (false === sessionInfo.valid) {
         throw new Meteor.Error(401, "invalid-session");
@@ -50,7 +72,6 @@ function loginWithToken (token) {
 }
 
 function loginWithCredentials ({username, password}) {
-    logFunc("loginWithCredentials");
     try {
         const result = HTTP.post("https://sso.innowatio.it/openam/json/authenticate", {
             headers: {
@@ -71,7 +92,6 @@ function loginWithCredentials ({username, password}) {
 }
 
 function invalidateToken (token) {
-    log("token", token);
     try {
         const result = HTTP.post(`https://sso.innowatio.it/openam/json/sessions/?_action=logout`, {
             headers: {
@@ -79,16 +99,14 @@ function invalidateToken (token) {
                 "iplanetDirectoryPro": token
             }
         });
-        log("result", result);
+        log.info({result}, "invalidateToken");
     } catch (error) {
         throw new Meteor.Error(500, "token-invalidation-failed");
     }
 }
 
 function retrieveUpsertUser (uid, token) {
-    logFunc("retrieveUpsertUser");
-    log("uid", uid);
-    log("token", token);
+    log.info({uid}, "retrieveUpsertUser");
     Meteor.users.upsert({
         "services.sso.uid": uid
     }, {
@@ -105,14 +123,4 @@ function retrieveUpsertUser (uid, token) {
     return {
         userId: user._id
     };
-}
-
-function log (name, message) {
-    // console.log(`============== ${name} =============`);
-    // console.log(message);
-    // console.log(`============= /${name} =============`);
-}
-
-function logFunc (func) {
-    // console.log(`=========================== ${func} ===========================`);
 }
