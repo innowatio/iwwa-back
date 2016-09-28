ed9b024838a9eed03d11916dc795655d39427ae7
Roberto Dell'Oro <roberto.delloro@mondora.com>
2016-09-28 10:57:28 +0200

    IWWA-371 - last update date in realtime page.

--- server/readings-real-time-aggregates/fixtures.js
+++ server/readings-real-time-aggregates/fixtures.js
@@ -6,13 +6,14 @@ Meteor.startup(() => {
         const path = "fixtures/readings-real-time-aggregates";
         const json = JSON.parse(Assets.getText(`${path}/test-realtime-aggregates.json`));
         const day = moment().format("YYYY-MM-DD");
+        const time = moment().format("x");
         json.map(realtime => {
             ReadingsRealTimeAggregates.insert({
                 _id: `${realtime.sensorId}-reading-${realtime.measurementType}`,
                 day: day,
                 measurementType : realtime.measurementType,
                 measurementValue : realtime.measurementValue,
-                measurementTime : realtime.measurementTime,
+                measurementTime : Number(time),
                 unitOfMeasurement : realtime.unitOfMeasurement,
                 sensorId : realtime.sensorId
             });
