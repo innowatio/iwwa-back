c7dfbed5c210efe6e708640f2e296b20021f7485
Davide Morri <davide.morri@mondora.com>
2016-10-10 19:40:57 +0900

    fix groups publication

--- server/groups/publications.js
+++ server/groups/publications.js
@@ -1,16 +1,5 @@
 Meteor.publishComposite("groups", {
     find: function () {
         return Groups.find();
-    },
-    children: [{
-        find: function (group) {
-            return Meteor.users.find({
-                groups: group.name
-            }, {
-                fields: {
-                    profile: 1
-                }
-            });
-        }
-    }]
+    }
 });
