d232e92b95a7078e608298ff6375628d21c5af35
Davide Morri <davide.morri@mondora.com>
2016-11-07 12:59:37 +0900

    clean and update roles management

--- server/groups/fixtures.js
+++ server/groups/fixtures.js
@@ -1,13 +1,13 @@
 Meteor.startup(() => {
-    if (
-        process.env.ENVIRONMENT !== "production" &&
-        Groups.find().count() === 0
-    ) {
-        JSON.parse(Assets.getText("fixtures/groups/groups.json")).map(groupName => {
+    if (Groups.find().count() === 0) {
+        JSON.parse(Assets.getText("fixtures/groups/admin.json")).map(groupName => {
             const group = {
                 name: groupName,
                 roles: JSON.parse(Assets.getText("fixtures/roles/roles.json"))
             };
+            Groups.insert(group);
+        });
+        JSON.parse(Assets.getText("fixtures/groups/groups.json")).map(group => {
             Groups.insert(group);
         });
     }
