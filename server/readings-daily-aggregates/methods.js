a3af56062f23614e33cf1df5c51b1c6561b6962d
Roberto Dell'Oro <roberto.delloro@mondora.com>
2016-12-23 12:28:47 +0100

    fixed issue on export

--- server/readings-daily-aggregates/methods.js
+++ server/readings-daily-aggregates/methods.js
@@ -1,7 +1,7 @@
 import {daysInInterval} from "../publications-commons";
 
-function getIds(sensorId, measurementsType, dateStart, dateEnd) {
-    return measurementsType.map(measurementType => {
+function getIds(sensorId, measurementTypes, dateStart, dateEnd) {
+    return measurementTypes.map(measurementType => {
         return daysInInterval(dateStart, dateEnd).map(day => `${sensorId}-${day}-reading-${measurementType}`);
     });
 }
@@ -14,11 +14,11 @@ Meteor.methods({
         });
 
         if (sensor) {
-            const measurementsType = sensor.measurementsType || [];
+            const measurementTypes = sensor.measurementTypes || [];
 
             const result = ReadingsDailyAggregates.find({
                 _id: {
-                    $in: _.flatten(getIds(sensorId, measurementsType, dateStart, dateEnd))
+                    $in: _.flatten(getIds(sensorId, measurementTypes, dateStart, dateEnd))
                 }
             }).fetch();
 
