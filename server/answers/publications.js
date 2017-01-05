9b273f247cc06dd599ffbd554c71864d6fee7a92
Davide Morri <davide.morri@mondora.com>
2017-01-05 18:05:46 +0900

    [IWWA-898]: fix answers publication to inspect user role

--- server/answers/publications.js
+++ server/answers/publications.js
@@ -1,10 +1,11 @@
+import {userHasRole} from "../publications-commons";
+
 Meteor.publish("answers", function ({siteId, category, type}) {
     var user = Meteor.users.findOne({_id: this.userId});
     if (!user) {
        return null;
     }
-    //TODO replace _.contains(user.groups, "admin") with permission role check
-    if (_.contains(user.sites, siteId) || _.contains(user.groups, "admin")) {
+    if (_.contains(user.sites, siteId) || userHasRole(user, "view-all-users")) {
        return Answers.find({
            _id: `${type}-${category}-${siteId}`
        });
