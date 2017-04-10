4fd65ea970d60c27a190eef3a3c62ca411a9a198
Manuel Serra <nitroin@hotmail.com>
2017-04-10 15:14:17 +0200

    [feat]: paginated sensors methods

---
+++ server/sensors/methods.js
@@ -0,0 +1,55 @@
+Meteor.methods({
+
+    getTags: function () {
+
+        const sensors = Sensors.find().fetch();
+
+        const tags = sensors.reduce((state, sensor) => {
+            return [...state, ...sensor.tags || []];
+        });
+
+        return _.uniq(tags);
+    },
+
+    getPrimaryTags: function () {
+        const sensors = Sensors.find().fetch();
+
+        const primaryTags = sensors.reduce((state, sensor) => {
+            return [...state, ...sensor.primaryTags || []];
+        });
+
+        return _.uniq(primaryTags);
+    },
+
+    getSensors: function(startIndex = 0, endIndex = 20, primaryTags, tags, search = "") {
+
+        const query = {
+            description: {
+                $regex: search
+            }
+        };
+
+        const searchTags = {
+            tags: {
+                $in: tags
+            }
+        };
+
+        const searchPrimaryTags = {
+            primaryTags: {
+                $in: primaryTags
+            }
+        };
+
+        const sensors = Sensors.find({
+            ...query,
+            ...tags && tags.length > 0 && searchTags,
+            ...primaryTags && primaryTags.length > 0 && searchPrimaryTags
+        }, {
+            skip: startIndex,
+            limit: endIndex - startIndex
+        }).fetch();
+
+        return sensors;
+    }
+})
